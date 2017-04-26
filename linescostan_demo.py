24f0f2bb35a017493e4ea86f9a49e048a0471766
mwdonoghue <mark.donoghue@netl.doe.gov>
2017-04-26 13:28:51 -0400

    cos/tan line with label demo

---
+++ linescostan_demo.py
@@ -0,0 +1,21 @@
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+mpl.rcParams['legend.fontsize'] = 10
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+theta = np.linspace(-4 * np.pi, 4 * np.pi, 100)
+z = np.linspace(-2, 2, 100)
+r = z**3 + 1
+x = r * np.tan(theta)
+y = r * np.cos(theta)
+ax.plot(x, y, z, label='label')
+ax.legend()
+
+#print matplotlib.projections.get_projection_names()
+
+plt.show()
